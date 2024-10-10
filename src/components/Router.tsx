ab248417a4bc5f23fe6e95de0dd4c375a3fc9897
yppelu <yppelu@gmail.com>
2024-10-10 14:56:24 +0700

    Add ErrorPage

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable sort-keys */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "@/components/App";
+import ErrorPage from "@/pages/ErrorPage/ErrorPage";
 
 function Router() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />
+      element: <App />,
+      errorElement: <ErrorPage />
     }
   ]);
 
---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,24 @@
+import classes from "@/pages/ErrorPage/ErrorPage.module.scss";
+import { Link } from "react-router-dom";
+
+function ErrorPage() {
+  document.title = "404 | Game Database";
+  return (
+    <>
+      <main className={classes.main}>
+        <h1 className={classes.heading}>404</h1>
+        <p className={classes.description}>
+          Whoops!
+          <br />
+          We couldn&apos;t find that page.
+        </p>
+        <Link className={classes.link} to="/">
+          Home page
+        </Link>
+      </main>
+      <div className={classes["background-image"]}></div>
+    </>
+  );
+}
+
+export default ErrorPage;
